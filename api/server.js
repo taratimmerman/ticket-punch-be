9de61ede58c71dc985a05afb84f3347502a2de9a
taratimmerman <timmermantara@gmail.com>
2021-07-14 16:52:04 -0600

    Created tickets router and endpoints

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const restrict = require('./middleware/restricted');
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
 const projectsRouter = require('./projects/projects-router');
-
+const ticketsRouter = require('./tickets/tickets-router');
 
 server.use(helmet());
 server.use(cors());
@@ -18,5 +18,6 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/users', restrict, usersRouter);
 server.use('/api/projects', restrict, projectsRouter);
+server.use('/api/tickets', restrict, ticketsRouter);
 
 module.exports = server;
