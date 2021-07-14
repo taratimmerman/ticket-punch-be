1618644364d2c470d65c74bf9beb6f2b9db0c986
taratimmerman <timmermantara@gmail.com>
2021-07-14 11:29:19 -0600

    Setup knex and db config. Made migrations for db tables. Made db cleanup seed.

---
+++ data/migrations/20210714163219_projects.js
@@ -0,0 +1,27 @@
+exports.up = (knex) => {
+    return knex.schema.createTable('projects', projects => {
+        projects.increments('id').primary();
+        projects.string('title', 30).notNullable();
+        projects.string('description', 140);
+        projects.enu('status', [
+            'stuck',
+            'working_on_it',
+            'done',
+        ])
+            .notNullable()
+            .defaultsTo('working_on_it');
+        projects
+            .integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        projects.timestamps(true, true);
+    });
+};
+
+exports.down = (knex) => {
+    return knex.schema.dropTableIfExists('projects');
+};
